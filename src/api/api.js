de8d33f6600c2df1a7df241cd16354df663df034
Sebona Haile <sebonahaile@Gmail.com>
2026-05-05 21:55:29 +0300

    Fix: FormData and screenshot upload added to Checkout

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
+
+// Kuni variable Vercel irraa dhufu fayyadama
+const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend URL
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  baseURL: `${API_URL}/api`,
 });
 
 export default api;
